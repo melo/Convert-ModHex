2842618d842b0e94dcc14a5a76bf862e544155cc
Pedro Melo <melo@simplicidade.org>
2011-11-12 17:38:04 +0000

    Added Convert::ModHex utilities and docs
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Convert/ModHex.pm
+++ lib/Convert/ModHex.pm
@@ -1,11 +1,85 @@
 package Convert::ModHex;
 
-# ABSTRACT: a very cool module
+# ABSTRACT: Conversion utilities between Yubico ModHex and hexa/decimal
 
 use strict;
 use warnings;
+use parent 'Exporter';
 
 # VERSION
 # AUTHORITY
 
+our @EXPORT_OK = qw( modhex2hex modhex2dec hex2modhex dec2modhex );
+
+sub modhex2hex {
+  my $m = $_[0];
+
+  $m =~ tr/cbdefghijklnrtuv/0123456789abcdef/;
+
+  return $m;
+}
+
+sub modhex2dec {
+  return hex(modhex2hex(@_));
+}
+
+sub hex2modhex {
+  my $m = $_[0];
+
+  $m =~ tr/0123456789abcdef/cbdefghijklnrtuv/;
+  $m = "c$m" if length($m) % 2;
+
+  return $m;
+}
+
+sub dec2modhex {
+  return hex2modhex(sprintf('%x', shift));
+}
+
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+    use Convert::ModHex qw( modhex2hex modhex2dec hex2modhex dec2modhex );
+    
+    $hex = modhex2hex($modhex);
+    $dec = modhex2dec($modhex);
+    
+    $modhex = hex2modhex($hex);
+    $modhex = dec2modhex($dec);
+
+
+=head1 DESCRIPTION
+
+This module provides utility functions that you can use to convert between
+L<http://www.yubico.com/modhex-calculator|ModHex encoding> (as used by the
+L<http://www.yubico.com/yubikey|Yubikey tokens>).
+
+
+=head1 FUNCTIONS
+
+=head2 modhex2hex
+
+Accepts a ModHex-encoded scalar, returns a hexadecimal-encoded scalar.
+
+
+=head2 modhex2dec
+
+Accepts a ModHex-encoded scalar, returns a numeric scalar.
+
+
+=head2 hex2modhex
+
+Accepts a hexadecimal scalar and returns the ModHex-encoded scalar.
+
+
+=head2 dec2modhex
+
+Accepts a numeric scalar and returns the ModHex-encoded scalar.
+
+
+=cut
